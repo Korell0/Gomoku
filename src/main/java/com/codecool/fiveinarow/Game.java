7625fe9112758b3fddbcf417b8c66f4908556d73
Tóth-Vargics Noémi <54523307+noemiboroka@users.noreply.github.com>
2020-03-12 12:28:22 +0100

    printScore function implemented

--- src/main/java/com/codecool/fiveinarow/Game.java
+++ src/main/java/com/codecool/fiveinarow/Game.java
@@ -5,12 +5,35 @@ public class Game implements GameInterface {
     private int[][] board;
     private int rows;
     private int cols;
+    int numberOfPlayers;
+    private Player[] players;
 
     public Game(int nRows, int nCols) {
-	board = new int [nRows][nCols];
-	rows = nRows;
-	cols = nCols;
+        board = new int[nRows][nCols];
+        numberOfPlayers = 2;
+        players = new Player[numberOfPlayers];
+
+        for(int index = 0; index < numberOfPlayers; index++){
+            players[index] = new Player();
+        }
+
+        rows = nRows;
+        cols = nCols;
+
     }
+
+    /*
+    public Game(int nRows, int nCols, int numberOfPlayers) {
+        board = new int[nRows][nCols];
+        players = new Player[numberOfPlayers];
+
+        rows = nRows;
+        cols = nCols;
+
+    }
+
+    */
+
 
     public int[][] getBoard() {
         return board;
@@ -42,18 +65,32 @@ public class Game implements GameInterface {
     public void printBoard() {
  	for (int indexRows = 0 ; indexRows < rows ; indexRows++ ){
 		for (int indexCols = 0 ; indexCols < cols ; indexCols++ ){
-			System.out.print(board[indexRows][indexCols]);	
+		    if(board[indexRows][indexCols] == 0)
+                {System.out.print(" x ");}
+
+            else if(board[indexRows][indexCols] == 1)
+                {System.out.print(" 1 ");}
+
+            else if(board[indexRows][indexCols] == 2)
+            {System.out.print(" 2 ");}
 		}
 	System.out.println();
 	}
     }
 
     public void printResult(int player) {
+
+        int score = this.players[player].getPlayerScore();
+
+        System.out.println("The " + player + ". player's score: " + score);
     }
 
     public void enableAi(int player) {
     }
 
     public void play(int howMany) {
+
+        printBoard();
+        printResult(0);
     }
 }
