45548a3001c03a9af13ce50986aa53392200acfd
Tóth-Vargics Noémi <54523307+noemiboroka@users.noreply.github.com>
2020-03-12 22:54:27 +0100

    Fixed isFull function

--- src/main/java/com/codecool/fiveinarow/Game.java
+++ src/main/java/com/codecool/fiveinarow/Game.java
@@ -52,9 +52,7 @@ public class Game implements GameInterface {
 
         for(int index = 0; index < coordinates.length; index++) {
 
-
             System.out.println("Player " + player  + ": " + "give us the " + coordinates[index]+ " coordinate:  ");
-
 
             int xCoordinate = scanner.nextInt();
             xCoordinate -= 1;
@@ -80,20 +78,18 @@ public class Game implements GameInterface {
     public boolean isFull() {
         boolean zeroFound = false;
 
+
         int xCoord = 0;
-        int yCoord = 0;
 
         while (xCoord < cols && !zeroFound){
 
+            int yCoord = 0;
             while (yCoord < rows && !zeroFound){
 
                 if(board[xCoord][yCoord] == 0){
                     zeroFound = true;
                 }
-
-                else{
-                    yCoord += 1;
-                }
+                yCoord += 1;
 
             }
 
@@ -112,12 +108,10 @@ public class Game implements GameInterface {
                 if ( board[indexRows][indexCols] == 0 ) {
                     System.out.print(" . ");
 
-
                 } else if ( board[indexRows][indexCols] == 1){
                     System.out.print(" X ");}
 
                     else if ( board[indexRows][indexCols] == 2){
-
                     System.out.print(" O ");
                 }
             }
@@ -149,31 +143,33 @@ public class Game implements GameInterface {
         while(!gameOver && !isFull()) {
 
             int[] playerMove = new int[3];
+                int player = 0;
+                while (player < players.length && !isFull()) {
 
-                for (int player = 0; player < players.length; player++) {
                     printBoard();
 
+                    do {
 
-                        do {
+                        playerMove = getMove(player + 1);
 
-                            playerMove = getMove(player + 1);
-
-                            if (spotTaken(playerMove[2], playerMove[1])) {
-                                System.out.println("That spot is already taken, chose another one!");
-                                System.out.println();
-                            }
+                        if (spotTaken(playerMove[2], playerMove[1])) {
+                            System.out.println("That spot is already taken, chose another one!");
+                            System.out.println();
                         }
+                    }
 
-                        while (spotTaken(playerMove[2], playerMove[1]));
+                    while (spotTaken(playerMove[2], playerMove[1]));
 
 
-                        mark(playerMove[0], playerMove[2], playerMove[1]);
+                    mark(playerMove[0], playerMove[2], playerMove[1]);
 
 
                     gameOver = hasWon(player, 5);
 
-
-            }
+                    player += 1;
+                    System.out.println(gameOver);
+                    System.out.println(isFull());
+                }
         }
 
     }
